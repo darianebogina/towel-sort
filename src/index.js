6b9b3ba32231be3ae0df52daf4fd5a299e1fc2ae
unknown <dasha.nebogina@mail.ru>
2024-08-18 22:32:27 +0300

    feat: add matrix to array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    let str = '';
+    for (let i = 0; i < matrix[0].length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            str += matrix[i][j];
+        }
+    }
+    return str.toArray();
 }
