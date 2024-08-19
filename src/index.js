d1fbf59b93bf115b0afaaff52ae33838cb95c2a6
unknown <dasha.nebogina@mail.ru>
2024-08-19 11:06:33 +0300

    fix: fix the way to get odd rows

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 module.exports = function towelSort (matrix) {
-    let array_odd = [];
-    let array_even = [];
+    let array = [];
     for (let i = 0; i < matrix.length; i++) {
         if ((i == 0) || (i % 2 == 0)){
-            for (let j = 0; j < matrix[0].length; j++) {
-                array_even.push(matrix[i][j]);
+            for (let j = 0; j < matrix[i].length; j++) {
+                array.push(matrix[i][j]);
             }
         }
         else {
-            for (let j = 0; j < matrix[0].length; j++) {
-                array_odd.push(matrix[i][j]);
+            for (let j = matrix[i].length - 1; j >= 0; j--) {
+                array.push(matrix[i][j]);
             }
-            array_odd.reverse();
         }
         
     }
-    return array_odd;
+    return array;
 }
