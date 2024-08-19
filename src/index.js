5264a1f67e791948061d10d412cbab2ad0bf1e16
unknown <dasha.nebogina@mail.ru>
2024-08-19 09:58:27 +0300

    feat: add code for odd rows

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 module.exports = function towelSort (matrix) {
-    let array = [];
+    let array_odd = [];
+    let array_even = [];
     for (let i = 0; i < matrix.length; i++) {
         if ((i == 0) || (i % 2 == 0)){
             for (let j = 0; j < matrix[0].length; j++) {
-                array.push(matrix[i][j]);
+                array_even.push(matrix[i][j]);
             }
         }
         else {
-            
+            for (let j = 0; j < matrix[0].length; j++) {
+                array_odd.push(matrix[i][j]);
+            }
+            array_odd.reverse();
         }
         
     }
-    return array;
+    return array_odd;
 }
